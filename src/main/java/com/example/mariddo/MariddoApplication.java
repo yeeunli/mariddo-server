c6a56033c465a84edc1707c4646c7c41438fec0a
yeeunli <lye920@g.hongik.ac.kr>
2024-12-08 14:57:53 +0900

    feat: 기본 개발 세팅

---
+++ src/main/java/com/example/mariddo/MariddoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mariddo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariddoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MariddoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mariddo/MariddoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mariddo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariddoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
